d4e8033ff5c7c3fc747de27d9e1c53e68949cee1
bharath kristipati <bharath.kumar.kristipati@oracle.com>
2017-07-10 20:00:44 +0530

    Updating the AdminLoginPage

--- src/test/java/com/covalant/automation/pages/AdminLoginPage.java
+++ src/test/java/com/covalant/automation/pages/AdminLoginPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 
 public class AdminLoginPage extends BasePage{
 
-	
+	//Hi This is comment
 	public void login(String user , String pass){
 		driver.findElement(By.id("input-username")).sendKeys(user);
 		driver.findElement(By.id("input-password")).sendKeys(pass);
